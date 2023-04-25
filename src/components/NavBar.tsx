e2c743e274377333b42b889ba91aadf38214a665
[talkingOrange] <sonrisa-bonita@naver.com>
2023-04-25 15:09:30 +0900

    [ feat ] router 설정 및 style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,18 +5,23 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link
-        href="/"
-        style={{ color: router.pathname === "/" ? "red" : "blue" }}
-      >
-        Home
+      <Link href="/" legacyBehavior>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
-      <Link
-        href="/about"
-        style={{ color: router.pathname === "/about" ? "red" : "blue" }}
-      >
-        about
+      <Link href="/about" legacyBehavior>
+        <a className={router.pathname === "/about" ? "active" : ""}>about</a>
       </Link>
+      <style jsx>
+        {`
+          nav {
+            background-color: tomato;
+          }
+          .active {
+            color: pink;
+            text-decoration: none;
+          }
+        `}
+      </style>
     </nav>
   );
 }
