1028bf699d5f0e69fc07152ad273362f5c5789b8
[talkingOrange] <sonrisa-bonita@naver.com>
2023-04-26 20:25:15 +0900

    [ feat ] layout 패턴 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <NavBar />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </div>
   );
 }
