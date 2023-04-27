2c07602215a365c1624c2b62bbbe2c0f9f15f760
[talkingOrange] <sonrisa-bonita@naver.com>
2023-04-27 22:45:43 +0900

    [ etc ] api 시도

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,25 @@ export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
+     // const { results } = await (await fetch(`/api/movies`)).json();
+      const { introoo } = await (
+        await fetch(`http://localhost:3000/api/intro`)
+      ).json();
+
+      console.log(introoo);
+   //   setMovies(results);
     })();
   }, []);
+
   return (
     <div>
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {/* {movies?.map((movie) => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
