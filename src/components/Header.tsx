805d35ea76cc7a64738f41f09f0096359393fdf2
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-03 11:47:27 +0900

    [ feat ] 다크모드 헤더에 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import DarkModeBtn from "./DarkModeBtn";
 import NavBar from "./NavBar";
 
 export default function Header() {
@@ -23,20 +24,7 @@ export default function Header() {
           <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
             <NavBar />
           </nav>
-          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <DarkModeBtn />
         </div>
       </header>
     </>
