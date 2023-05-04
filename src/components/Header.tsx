6a9321717fb51995e1427cca9da1b6f7f1c92ba0
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-05 02:20:47 +0900

    [ feat ] 헤더수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,20 @@
 import DarkModeBtn from "./DarkModeBtn";
 import NavBar from "./NavBar";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <>
       <header class="text-gray-600 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span class="ml-3 text-xl">YEWON PORTFOLIO</span>
+          <a href="/about" class="flex items-center mb-4 md:mb-0">
+            <Image
+              src="/profileImg.jpg"
+              alt="Picture of the author"
+              width={40} //automatically provided
+              height={40} //automatically provided
+              class="rounded-full"
+            />
           </a>
           <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
             <NavBar />
