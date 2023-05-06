2d5630c173f3143c8d8d51b54c997157d1edfc9a
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-06 18:36:07 +0900

    [ feat ] 배포오류해결 / className으로 변경

--- src/components/about/Non-DevelopmentExperiences.tsx
+++ src/components/about/Non-DevelopmentExperiences.tsx
@@ -1,36 +1,36 @@
 export default function NonDevelopmentExperiences() {
   return (
     <>
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <h1 class="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
             🤗 Non-Development Experiences
           </h1>
-          <div class="flex flex-wrap -m-4">
-            <div class="p-4 md:w-1/2 w-full">
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                  <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 md:w-1/2 w-full">
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                 </div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
                   <svg
                     fill="none"
                     stroke="currentColor"
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
                     <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                   </svg>
                 </div>
-                <div class="flex-grow pl-4">
+                <div className="flex-grow pl-4">
                   <h4>단기교환학생/오스트리아 빈대학교</h4>
-                  <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
                     2023.02.07 ~ 2022.02.24
                   </h2>
-                  <p class="leading-relaxed">
+                  <p className="leading-relaxed">
                     <ul>성신여자대학교 오스트리아 빈 대학교 단기 교환학생 </ul>
                     <li>독일어 공부</li>
                     <li>문화 탐색</li>
@@ -38,29 +38,29 @@ export default function NonDevelopmentExperiences() {
                 </div>
               </div>
 
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                  <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                 </div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
                   <svg
                     fill="none"
                     stroke="currentColor"
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                   </svg>
                 </div>
-                <div class="flex-grow pl-4">
+                <div className="flex-grow pl-4">
                   <h4>성신여자대학교 아동발달 연구실 RA</h4>
-                  <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
                     2021.09.01 ~ 2022.02.15{" "}
                   </h2>
-                  <p class="leading-relaxed">
+                  <p className="leading-relaxed">
                     <ul>
                       성신여자대학교 심리학과 소속 아동 발달 연구실의 학부생
                       연구조교
@@ -71,30 +71,30 @@ export default function NonDevelopmentExperiences() {
                   </p>
                 </div>
               </div>
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                  <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                 </div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
                   <svg
                     fill="none"
                     stroke="currentColor"
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
                     <circle cx="12" cy="5" r="3"></circle>
                     <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
                   </svg>
                 </div>
-                <div class="flex-grow pl-4">
+                <div className="flex-grow pl-4">
                   <h4>마인즈랩 서포터즈</h4>
-                  <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
                     2021.07.05 ~ 2021.08.31
                   </h2>
-                  <p class="leading-relaxed">
+                  <p className="leading-relaxed">
                     <ul>마인즈랩에서 진행하는 회사 홍보 서포터즈</ul>
                     <li>마인즈랩 AI 감정분석, AI Voice 사용 경험</li>
                     <li>카드뉴스, 영상 제작</li>
@@ -103,30 +103,30 @@ export default function NonDevelopmentExperiences() {
                 </div>
               </div>
             </div>
-            <div class="p-4 md:w-1/2 w-full">
-              <div class="flex relative pb-12">
-                <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                  <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            <div className="p-4 md:w-1/2 w-full">
+              <div className="flex relative pb-12">
+                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                  <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                 </div>
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
                   <svg
                     fill="none"
                     stroke="currentColor"
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
                     <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                   </svg>
                 </div>
-                <div class="flex-grow pl-4">
+                <div className="flex-grow pl-4">
                   <h4>서울시자원봉사센터 프로청사이</h4>
-                  <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
                     2020.02.07 ~ 2020.10.08{" "}
                   </h2>
-                  <p class="leading-relaxed">
+                  <p className="leading-relaxed">
                     <ul>서울시자원봉사센터에서 진행하는 봉사활동</ul>
                     <li>카드뉴스 제작 - 제로웨이스트 물품 소개 </li>
                     <li>팀 협업 경험</li>
@@ -134,27 +134,27 @@ export default function NonDevelopmentExperiences() {
                   </p>
                 </div>
               </div>
-              <div class="flex relative">
-                <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+              <div className="flex relative">
+                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
                   <svg
                     fill="none"
                     stroke="currentColor"
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
                     <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                     <path d="M22 4L12 14.01l-3-3"></path>
                   </svg>
                 </div>
-                <div class="flex-grow pl-4">
+                <div className="flex-grow pl-4">
                   <h4>마인드카페 리스너 4기</h4>
-                  <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                  <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
                     2020.02.27 ~ 2020.04.27{" "}
                   </h2>
-                  <p class="leading-relaxed">
+                  <p className="leading-relaxed">
                     <ul>
                       심리상담 어플 ‘마인드카페’에서 소통과 공감을 하는 리더
                       역할
