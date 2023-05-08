a90b0098fb0f0e1aad358925c3e83a2dfda92293
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-08 17:10:28 +0900

    [ feat ] 자잘한 스타일 내용 수정

--- src/components/about/Education.tsx
+++ src/components/about/Education.tsx
@@ -1,38 +1,37 @@
 export default function Education() {
   return (
     <>
-    <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font">
         <div className="container px-10 py-20 mx-auto">
-              <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center border-b-2 border-gray-100 pb-5">
-                👩‍🎓 Education
-              </h1>
+          <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center border-b-2 border-gray-100 pb-5">
+            👩‍🎓 Education
+          </h1>
 
-              <h2 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                성신여자대학교
-              </h2>
-              <div className="flex mb-4">
-                <span className="text-gray-600 ml-3">
-                  2019.03 ~ 2023.08.(졸업예정)
-                </span>
-              </div>
-              <p className="leading-relaxed">
-                <ul>
-                  <li>- 심리학 전공</li>
-                  <li>- 컴퓨터공학 복수전공</li>
-                </ul>
-              </p>
+          <h2 className="text-gray-900 text-3xl title-font font-medium mb-1">
+            성신여자대학교
+          </h2>
+          <div className="flex mb-4">
+            <span className="text-gray-600 ml-3">
+              2019.03 ~ 2023.08.(졸업예정)
+            </span>
+          </div>
+          <p className="leading-relaxed">
+            <ul>
+              <li>- 심리학 전공</li>
+              <li>- 컴퓨터공학 복수전공</li>
+            </ul>
+          </p>
 
-              <h2 className="text-gray-900 text-2xl title-font font-medium mb-1">
-                Scholarship
-              </h2>
+          <h2 className="text-gray-900 text-2xl title-font font-medium mb-1">
+            Scholarship
+          </h2>
 
-              <p className="leading-relaxed">
-                <ul>
-                  <li>- [2019~2023] 입학성적우수자 전액장학금</li>
-                  <li>- [2021-2] 성신장학금(학업성적우수)</li>
-                </ul>
-              </p>
-
+          <p className="leading-relaxed">
+            <ul>
+              <li>- [2019~2023] 입학성적우수자 전액장학금</li>
+              <li>- [2021-2] 성신장학금(학업성적우수)</li>
+            </ul>
+          </p>
         </div>
       </section>
     </>
