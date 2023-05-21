c7b209aa3bbe6f601992b337dfd6396ca4081e89
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-21 15:43:20 +0900

    [ feat ] notion_token 상수 설정

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -16,14 +16,15 @@ export default function project({ projects }: any) {
 //서버에서 찍히는 부분
 export async function getStaticProps() {
   const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
+  const NOTION_TOKEN = process.env.NOTION_TOKEN;
+
   const options = {
     method: "POST",
     headers: {
       accept: "application/json",
       "Notion-Version": "2022-06-28",
       "Content-Type": "application/json",
-      Authorization:
-        "Bearer secret_vlMd7yCIqLbNC8RiDIXzeFBEcPJNpIeUTW4zrU1kgKb",
+      Authorization: `Bearer ${NOTION_TOKEN}`,
     },
     body: JSON.stringify({ page_size: 100 }),
   };
