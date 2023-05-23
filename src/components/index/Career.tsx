ab7d98c425e1a54abd253d4adae6a95453a4fd65
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-23 21:11:40 +0900

    [ style ] 커리어 스타일 변경

--- src/components/index/Career.tsx
+++ src/components/index/Career.tsx
@@ -2,7 +2,7 @@ export default function Career() {
   return (
     <>
       <div className="py-6 sm:py-0 bg-black  bg-opacity-100 -mt-20 relative z-50">
-        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-44">
           <div className="grid gap-8 sm:grid-cols-2 sm:gap-12">
             <div className="flex flex-col items-center justify-center sm:items-start md:py-6 lg:py-12 xl:py-24">
               <p className="mb-4 text-sm font-semibold uppercase text-indigo-500 md:text-base">
@@ -13,24 +13,23 @@ export default function Career() {
               </h1>
 
               <p className="mb-4 text-center text-white sm:text-left md:text-lg">
-                2022.09.01 ~ 2022.12.31 (인턴)
+                2022.09.01 ~ 2022.12.31 / 인턴
                 <ul>
-                  <li>1. 웹 퍼블리싱 업무</li>
+                  <li className="mt-5 mb-1">1. 웹 퍼블리싱 업무</li>
                   <ul>
                     <li>- 고객사 요청 팝업 퍼블리싱</li>
                     <li>- 벤치마킹 자료 탐색 및 분류</li>
                   </ul>
-                  <li>
+                  <li className="mt-5 mb-1">
                     2. 자사 에디터 웹 사이트 오류 수정 ( JavaScript )
                     <ul>
                       <li>- 오류 확인 및 오류 시트 정리</li>
                       <li>
-                        - 이미지 에디터, 모션, 표, 라인, 폰트 등 에디터 오류
-                        수정
+                        - 이미지 에디터, 모션, 표, 라인, 폰트 등 오류 수정
                       </li>
                     </ul>
                   </li>
-                  <li>
+                  <li className="mt-5 mb-1">
                     3. 자사 에디터 웹 사이트 기능 추가 ( JavaScript, PHP )
                     <ul>
                       <li>- 드래그 앤 드랍 기능 추가</li>
